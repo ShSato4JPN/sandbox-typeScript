692e8ef36360ae5278860042e7f7a15e40157a44
ShSato4 <shsato4@ShSato4noMacBook-Air.local>
2022-04-07 22:58:35 +0900

    react sampel commit

---
+++ sample-react/sample01/src/components/thead.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const Component: React.FC = () => (
+  <thead>
+    <tr>対象世代</tr>
+    <th>取り組んでいる</th>
+    <th>近いうちに取り組みたい</th>
+    <th>取り組んでいない</th>
+    <th>必要ない</th>
+  </thead>
+)
+
+export default Component
